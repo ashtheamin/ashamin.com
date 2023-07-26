66cef55cd17974c684744c8801667afcd19315b1
Ash Amin <ashtheamin@gmail.com>
2023-07-26 19:53:33 +1000

    Update theme toggle

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,35 @@
-document.getElementById("html").style.color = "red";
+function applytheme() {
+    if (localStorage.getItem("theme") === "light") {
+        const html = document.getElementById("html");
+        html.style.color = "#2C1728";
+        html.style.backgroundColor = "#FFDDF2";
+    }
+
+    if (localStorage.getItem("theme") === "dark") {
+        const html = document.getElementById("html");
+        html.style.color = "#FFDDF2";
+        html.style.backgroundColor = "#2C1728";
+    }
+}
+
+function init() {
+    function toggletheme() {
+        console.log('button hit');
+        if (localStorage.getItem("theme") === "light") {
+            localStorage.setItem("theme", "dark");
+        }
+        else {
+            localStorage.setItem("theme", "light");
+        }
+        applytheme();
+    }
+    document.getElementById("buttontoggle").addEventListener("click", toggletheme, true)
+}
+
+if (localStorage.getItem("theme") == "") {
+    localStorage.setItem("theme") = "dark";
+}
+
+document.addEventListener('DOMContentLoaded', init, init);
+applytheme();
+
